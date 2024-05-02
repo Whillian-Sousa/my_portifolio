18e9a28af336bea568f7f22ed11d9e3d54992b9b
WhillianSousa <whillscf@gmail.com>
2024-05-02 18:52:24 -0300

    Create a Work section.

---
+++ src/components/Work.jsx
@@ -0,0 +1,17 @@
+import Heading from "./Heading";
+import Section from "./Section";
+
+const Work = () => {
+  return (
+    <Section id="work" className="overflow-hidden">
+      <div className="contaier md:pb-10">
+        <Heading
+          title="Choose the best for your project"
+          tag="What i'am working on"
+        />
+      </div>
+    </Section>
+  );
+};
+
+export default Work;
