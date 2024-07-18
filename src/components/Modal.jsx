98bd745a3116e7c2927075e29a740a3f7aebe128
WhillianSousa <whillscf@gmail.com>
2024-07-17 21:20:48 -0300

    fix dialog modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,11 +4,11 @@ const Modal = ({ open, onClose, children }) => {
   return (
     <div
       onClick={onClose}
-      className={`absolute overflow-hidden z-1 inset-0 flex justify-center items-center transition-colors ${open ? "visible backdrop-blur-sm" : "invisible"}`}
+      className={`absolute overflow-hidden z-1 inset-0 flex justify-center items-center transition-colors ${open ? "visible backdrop-blur" : "invisible"}`}
     >
       <div
         // onClick={(e) => e.stopPropagation()}
-        className={`rounded-xl shadow p-6 transition-all ${open ? "scale-100 opacity-100" : "scale-125 opacity-0"}`}
+        className={`rounded-xl bg-n-7/70 shadow p-6 transition-all ${open ? "scale-100 opacity-100" : "scale-125 opacity-0"}`}
       >
         <div className="absolute inset-0 bg-noisy-texture opacity-10" />
         {children}
